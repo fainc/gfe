97781c89bc171f8362fcce7e579d7fc4d1f3e0e4
lin <zx@lendata.net>
2023-09-27 12:12:14 +0800

    init response

---
+++ go.mod
@@ -0,0 +1,36 @@
+module github.com/fainc/gfe
+
+go 1.18
+
+require (
+	github.com/fainc/go-crypto v0.0.1
+	github.com/gogf/gf/v2 v2.5.4
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.0 // indirect
+	github.com/clbanning/mxj/v2 v2.7.0 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/grokify/html-strip-tags-go v0.0.1 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/tjfoc/gmsm v1.4.1 // indirect
+	go.opentelemetry.io/otel v1.14.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.14.0 // indirect
+	go.opentelemetry.io/otel/trace v1.14.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ response/handler.go
@@ -0,0 +1,142 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/fainc/go-crypto/gm"
+	"github.com/fainc/go-crypto/rsa"
+	"github.com/gogf/gf/v2/encoding/gjson"
+	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/text/gstr"
+	"github.com/gogf/gf/v2/util/gmeta"
+)
+
+// HandlerResponse 默认数据返回中间件
+func HandlerResponse(r *ghttp.Request) {
+	var (
+		ctx  = r.Context()
+		err  = r.GetError()
+		res  = r.GetHandlerResponse()
+		code = gerror.Code(err)
+	)
+
+	// openapi
+	if gstr.Contains(r.RequestURI, "api.json") {
+		return
+	}
+
+	// 已有err错误
+	if err != nil {
+		if code.Code() == 50 || code.Code() == 52 || code.Code() == 500 { // 服务器错误
+			Json().InternalError(ctx, g.I18n().Translate(ctx, "InternalError"))
+			return
+		}
+		if code.Code() == 401 { // 登录
+			Json().UnAuthorizedError(ctx, code.Message(), code.Detail())
+			return
+		}
+		if code.Code() == 402 { // 解密
+			Json().DecryptError(ctx, code.Message(), code.Detail())
+			return
+		}
+		if code.Code() == 403 { // 签名
+			Json().SignatureError(ctx, code.Message(), code.Detail())
+			return
+		}
+		if code.Code() == 404 { // 404
+			Json().NotFoundError(ctx, code.Message())
+			return
+		}
+		Json().Error(ctx, err.Error(), code.Code(), code.Detail()) // 常规错误
+		return
+	}
+	// 已退出程序流程，不走当前中间件
+	if r.IsExited() {
+		return
+	}
+
+	// 自定义输出
+	if gmeta.Get(res, "mime").String() == "custom" {
+		return
+	}
+
+	// 状态码正常且已有buffer内容
+	if r.Response.Status == http.StatusOK && r.Response.BufferLength() > 0 {
+		return
+	}
+
+	// 已有异常响应状态码
+	if r.Response.Status > 0 && r.Response.Status != http.StatusOK {
+		switch r.Response.Status {
+		case http.StatusNotFound: // 404
+			Json().NotFoundError(ctx, g.I18n().Translate(ctx, "NotFound"))
+			return
+		case http.StatusUnauthorized: // 401
+			return
+		case http.StatusBadRequest: // 400
+			return
+		default:
+			Json().InternalError(ctx, g.I18n().Translate(ctx, "InternalError"))
+			return
+		}
+	}
+
+	/*CTX判断是否加密 etc.*/
+	// r.SetCtxVar("response_encrypt", true)
+	// r.SetCtxVar("response_encrypt_algorithm", cert.Algorithm)
+	// r.SetCtxVar("response_encrypt_key", cert.Public)
+	// r.SetCtxVar("response_encrypt_hex", true)
+	var (
+		encrypt = r.GetCtxVar("response_encrypt", false).Bool()
+	)
+	if !encrypt || res == nil { // CTX声明不加密或空数据时不处理加密，直接返回
+		Json().Success(ctx, res)
+		return
+	}
+	var (
+		encryptAlgorithm = r.GetCtxVar("response_encrypt_algorithm", "").String()
+	)
+	if encrypt && encryptAlgorithm == "" { // CTX加密算法为空
+		Json().InternalError(ctx, g.I18n().Translate(ctx, "UnsupportedEncryptAlgorithm"))
+		return
+	}
+	var (
+		encryptKey     = r.GetCtxVar("response_encrypt_key", "").String()
+		encryptHex     = r.GetCtxVar("response_encrypt_hex", false).Bool() // 是否使用hex，否则输出base64
+		encryptSM2Mode = r.GetCtxVar("response_encrypt_sm2_mode", 0).Int()
+		encryptSM4Mode = r.GetCtxVar("response_encrypt_sm4_mode", "ECB").String()
+	)
+	if encryptKey == "" { // CTX加密密钥/证书为空
+		Json().InternalError(ctx, g.I18n().Translate(ctx, "EncryptKeyError"))
+		return
+	}
+	switch encryptAlgorithm {
+	case "SM2":
+		res, err = gm.Sm2().Encrypt(encryptKey, gjson.MustEncodeString(res), encryptHex, encryptSM2Mode)
+		if err != nil {
+			Json().InternalError(ctx, g.I18n().Translate(ctx, "DataEncryptFailed"))
+			return
+		}
+	case "SM4":
+		res, err = gm.Sm4().Encrypt(encryptSM4Mode, encryptKey, gjson.MustEncodeString(res), encryptHex)
+		if err != nil {
+			Json().InternalError(ctx, g.I18n().Translate(ctx, "DataEncryptFailed"))
+			return
+		}
+	case "RSA_PKCS1":
+		res, err = rsa.Encrypt(gjson.MustEncodeString(res), encryptKey, encryptHex)
+		if err != nil {
+			Json().InternalError(ctx, g.I18n().Translate(ctx, "DataEncryptFailed"))
+			return
+		}
+	default:
+		Json().InternalError(ctx, g.I18n().Translate(ctx, "UnsupportedEncryptAlgorithm"))
+		return
+	}
+	Json(JsonOptions{
+		Encrypt:          encrypt,
+		EncryptAlgorithm: encryptAlgorithm,
+	}).Success(ctx, res)
+}
