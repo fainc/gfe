09f7974802a3451c07632a251873f427de3c0d2c
PickMeUp <zx@lendata.net>
2023-11-21 17:12:06 +0800

    新增 runtime helper

---
+++ helper/runtime.go
@@ -0,0 +1,33 @@
+package helper
+
+import (
+	"context"
+	"time"
+
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gtime"
+)
+
+type runTime struct {
+	t time.Time
+}
+
+func RunTime() *runTime {
+	return &runTime{}
+}
+
+// GetRequestRunTime 获取请求进入框架后的实时运行时间
+func (rec *runTime) GetRequestRunTime(ctx context.Context) int64 {
+	r := g.RequestFromCtx(ctx)
+	return gtime.Now().TimestampMilli() - r.EnterTime
+}
+
+// Start 开始计算
+func (rec *runTime) Start() {
+	rec.t = time.Now()
+}
+
+// Since 获取开始计算时间至当前时间用时
+func (rec *runTime) Since() time.Duration {
+	return time.Since(rec.t)
+}
