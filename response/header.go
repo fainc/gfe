bd391219141a8d6d488f165bb981763744a14d2a
x <x@lendata.net>
2024-03-12 11:01:18 +0800

    update response.SetDefaultResponseHeader,keep server fingerprints safety.

--- response/header.go
+++ response/header.go
@@ -1,7 +1,10 @@
 package response
 
 import (
+	"context"
 	"os"
+
+	"github.com/gogf/gf/v2/frame/g"
 
 	"github.com/gogf/gf/v2/crypto/gmd5"
 	"github.com/gogf/gf/v2/i18n/gi18n"
@@ -10,10 +13,16 @@ import (
 	"github.com/gogf/gf/v2/util/gconv"
 )
 
-// SetDefaultResponseHeader 设置公共响应头
+var agent = g.Cfg().MustGet(context.Background(), "server.serverAgent").String()
+
 func SetDefaultResponseHeader(r *ghttp.Request) {
 	serverName, _ := os.Hostname()
 	serverID, _ := gmd5.Encrypt(serverName)
+	if agent == "" {
+		// Keep server fingerprints safety.
+		// Overwrite the default server agent "GoFrame HTTP Server".
+		r.Server.SetServerAgent("Unknown")
+	}
 	r.Response.Header().Set("Server-Id", serverID)
 	r.Response.Header().Set("Server-Lang", gi18n.LanguageFromCtx(r.Context()))
 	r.Response.Header().Set("Server-RunTime", gconv.String(gtime.Now().TimestampMilli()-r.EnterTime))
